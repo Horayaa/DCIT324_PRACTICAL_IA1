be4ffb72ceb767342b897cc582913f8b88948b0f
Horayaa <razakhoraya9@gmail.com>
2026-08-11 15:24:42 +0000

    Add files via upload
    Complete DCIT 324 pratical  IA1

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import DetailsScreen from './screens/DetailsScreen';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>DCIT324 PRACTICAL IA</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeMain"
+        component={HomeScreen}
+        options={{ title: 'Campus News' }}
+      />
+      <HomeStack.Screen
+        name="NewsDetails"
+        component={DetailsScreen}
+        options={{ title: 'News Details' }}
+      />
+    </HomeStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="HomeTab"
+          component={HomeStackScreen}
+          options={{ headerShown: false, title: 'Home' }}
+        />
+        <Tab.Screen
+          name="AboutTab"
+          component={AboutScreen}
+          options={{ title: 'About' }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
